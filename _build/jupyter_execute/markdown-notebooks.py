3d675f45927ce04a784163e24a7c4e0e04d8f9b6
sebas <sebas19022016@gmail.com>
2022-05-03 21:36:15 -0500

    adding my first book!

---
+++ _build/jupyter_execute/markdown-notebooks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Ferrari
+# Ferrari es una compañía de automóviles deportivos con sede en Maranello-Módena, Emilia-Romaña, Italia. Este fabricante es el resultante de la idea de su fundador Enzo Ferrari de crear una marca de automóviles de turismo con prestaciones lujosas y deportivas, basándose en los éxitos obtenidos a partir de la creación de su Scuderia Ferrari de automovilismo en 1929.
+# 
+# ## Ferrari F40
+# El Ferrari F40 es un automóvil superdeportivo berlinetta de 2 puertas biplaza, producido por el fabricante de automóviles italiano Ferrari. Su fabricación comenzó en 1987 con motivo del 40.º aniversario de la fundación de la marca.
+# 
+# Su precio, su comportamiento y su concepto lo convirtieron en un clásico instantáneo y hoy en día, es recordado junto al 288 GTO como el último gran Ferrari de la vieja escuela.
+# 
+# Un ejemplar que perteneció al piloto de Fórmula 1, Nigel Mansell, fue vendido en 1990 por un precio récord de 1 000 000 £, el cual se mantuvo hasta los años 2010.
+# 
+# [**Esto te llevara a concoer mas sobre la historia**](https://es.wikipedia.org/wiki/Ferrari_F40)
+# ![ferrari f40](f40.JPEG)
---
+++ _build/jupyter_execute/notebooks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Lamborghini
+# Automobili Lamborghini Holding S.p.A., comúnmente conocido como Lamborghini en idioma español, usualmente lamborghini es un fabricante italiano de automóviles superdeportivos fundado en 1963 por el fabricante de tractores Ferruccio Lamborghini con sede y oficinas en Bolognese y que actualmente es parte de la matriz de Volkswagen. Algunos de los nombres de sus automóviles hacen referencia a nombres de toros bravos de lidia indultados (Murciélago), famosos o históricos (Diablo, Aventador), o simplemente palabras relacionadas con la tauromaquia (Miura, Espada, Urraco). Ferruccio se instaló en Sant'Agata Bolognese, en Bolonia, a pocos kilómetros de Maranello, para rivalizar con su vecino Enzo Ferrari, a raíz de una discusión por la calidad de sus coches. La empresa original se llamaba Automobili Ferruccio Lamborghini SpA, que derivaba a su vez de la Lamborghini Trattori S.A.
+# 
+# ## Lamborghini Aventador
+# 
+# El Lamborghini Aventador es un automóvil superdeportivo biplaza de dos puertas de tijera, con motor central-trasero montado longitudinalmente y tracción en las cuatro ruedas, producido por el fabricante italiano Lamborghini, filial del Grupo Volkswagen. Fue concebido para reemplazar al Murciélago, como el nuevo modelo tope de gama.
+# 
+# [**Esto te llevara a concoer mas sobre la historia**](https://es.wikipedia.org/wiki/Lamborghini_Aventador)
+# ![lamborghini aventador](aventador.JPEG)
